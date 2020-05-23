afb8b27f80a52119e2d15935c492ad851bfdd864
samael.simoes <samael.simoes@gmail.com>
2020-05-23 16:28:40 -0300

    finished chess project

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,27 +1,34 @@
 package boardgame;
 
-public class Piece {
-	
+public abstract class Piece {
+
 	protected Position position;
 	private Board board;
 	
-	/*
-	 * Passando apenas o tabuleiro na hora de cria a peça
-	 * a posição inicial de uma peça será nula
-	*/
 	public Piece(Board board) {
 		this.board = board;
 		position = null;
 	}
 
-	/*
-	 *  Retirei o set para o tabuleiro não seja alterado somente classe
-	 *  Somente class dentro do mesmo pacote e subclasses podera acessar o tabuleiro de uma peça
-	*/
 	protected Board getBoard() {
 		return board;
 	}
-
 	
+	public abstract boolean[][] possibleMoves();
 	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,8 +1,7 @@
 package boardgame;
 
-//Camada tabuleiro
 public class Position {
-	
+
 	private int row;
 	private int column;
 	
@@ -27,12 +26,13 @@ public class Position {
 		this.column = column;
 	}
 	
-	/*
-	 * IMPRIMIR LINHA E COLUNA
-	*/
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
 	@Override
 	public String toString() {
 		return row + ", " + column;
 	}
-	
 }
